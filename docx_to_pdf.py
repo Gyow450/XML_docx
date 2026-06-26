98bc33aa581218464327e2de9124d626bc5db47f
Gyow450 <1752708214@qq.com>
2026-06-26 23:04:00 +0800

    change(docx_to_pdf.py):调整到新版互动

--- docx_to_pdf.py
+++ docx_to_pdf.py
@@ -1,7 +1,10 @@
-import win32com.client as win32
+import sys
 from pathlib import Path
-from src import interraction_terminal
+
 from fastprogress import progress_bar
+import win32com.client as win32
+
+from src.interraction_terminal import set_local_setting
 
 def docx_transform(input_dir:Path|str,output_dir:Path|str)->None:
     """将输入文件夹中的docx文件批量转为pdf文件，另存至输出文件夹内"""
@@ -40,12 +43,21 @@ def docx_transform(input_dir:Path|str,output_dir:Path|str)->None:
     word.Quit()
 
 if __name__ == '__main__':
-    base_set={
-        (0,'数据源','',r'E:\BaiduSyncdisk\成渝特检\成华区2026年老化评估项目\输出'),
-        (0,'保存目标','',r'E:\BaiduSyncdisk\成渝特检\成华区2026年老化评估项目\输出'),
-    } 
-    SET_DICT = interraction_terminal.set_argumments(base_set)
-    INPUT_DIR = Path(SET_DICT['数据源'])
-    OUTPUT_DIR = Path(SET_DICT['保存目标'])
+    # 预置参数
+    # 类型码：0-文件夹，1-保存文件，2-打开文件，3-布尔，4-字符串
+    setting_dict={
+        '源文件夹':[0,'',''],
+        '输出文件夹':[0,'',''],
+    }
+    
+    # 设置路径等参数，优先读取之前保存的参数
+    script_name=Path(__file__).resolve().stem
+    CONFIG=set_local_setting(script_name,setting_dict)
+    
+    if CONFIG is None:
+        sys.exit(0)
+    
+    INPUT_DIR = Path(CONFIG['源文件夹'])
+    OUTPUT_DIR = Path(CONFIG['保存目标'])
     docx_transform(INPUT_DIR,OUTPUT_DIR)
     
